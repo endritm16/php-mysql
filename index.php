c5bb307c5ab77a3c30b4b10ed0cec06b98966524
endritm16 <emehmeti805@gmail.com>
2025-01-25 07:41:15 -0800

    first index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// $school = "Digital School";
+// $age = 22;
+
+// echo "I like $school"
+// echo "My age is $age";
+
+// $x = 120;
+// $y = 20;
+
+// echo $x * $y
+
+$name = "Endrit";
+$surname = "Mehmeti";
+
+$fullname = $name . " " . $surname;
+
+echo $fullname;
+?>
+
+
