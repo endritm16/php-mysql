3ce6d1c0c7bd71f05101f7b8884bb977228bb9a5
endritm16 <emehmeti805@gmail.com>
2025-04-26 07:43:24 -0700

    form name added

---
+++ Day 10/config.php
@@ -0,0 +1,14 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'dbtest';
+
+try {
+    $connect = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+  
+}catch(Exception $e){
+        echo "Something went wrong";
+}
+
+?>
---
+++ Day 10/register.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once('config.php');
+
+    if(isset($_POST))
