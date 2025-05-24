f6bcb6c67a7326e4652af48902c7be61f4f4ab3c
endritm16 <emehmeti805@gmail.com>
2025-05-24 16:42:20 +0200

    a

---
+++ MMS/config.php
@@ -0,0 +1,14 @@
+<?php
+$user = "root"
+$pass = "";
+$server = "localhost";
+$dbname = "mms";
+
+try{
+    $conn = new PDO("mysql:host=$server; dbname = $dbname", $user, $pass);
+
+}catch (PDOExpection $e){
+    echo 'error: '. $e->getMessage();
+}
+
+?>
---
+++ MMS/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+	<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+    <title>Document</title>
+    <style>
+html, body {
+    height: 100%;
+}
+body {
+display: flex;
+align-items: center;
+padding-top: 40px;
+padding-bottom: 40px;
+background-color: #f5f5f5;
+
+}
+
+
+
+    </style>
+</head>
+<body>
+   
+
+
+</body>
+</html>
+
+
+
